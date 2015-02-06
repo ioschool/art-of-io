8464f8264c9bdae64fd986e04a055b6fca9aa847
Michael Williams <dinosaur@riseup.net>
2015-02-06 19:26:12 +1300

    Updates

---
+++ bundle.js
@@ -0,0 +1,355 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+module.exports = function() {
+  return function(deck) {
+    var backdrops;
+
+    function createBackdropForSlide(slide) {
+      var backdropAttribute = slide.getAttribute('data-bespoke-backdrop');
+
+      if (backdropAttribute) {
+        var backdrop = document.createElement('div');
+        backdrop.className = backdropAttribute;
+        backdrop.classList.add('bespoke-backdrop');
+        deck.parent.appendChild(backdrop);
+        return backdrop;
+      }
+    }
+
+    function updateClasses(el) {
+      if (el) {
+        var index = backdrops.indexOf(el),
+          currentIndex = deck.slide();
+
+        removeClass(el, 'active');
+        removeClass(el, 'inactive');
+        removeClass(el, 'before');
+        removeClass(el, 'after');
+
+        if (index !== currentIndex) {
+          addClass(el, 'inactive');
+          addClass(el, index < currentIndex ? 'before' : 'after');
+        } else {
+          addClass(el, 'active');
+        }
+      }
+    }
+
+    function removeClass(el, className) {
+      el.classList.remove('bespoke-backdrop-' + className);
+    }
+
+    function addClass(el, className) {
+      el.classList.add('bespoke-backdrop-' + className);
+    }
+
+    backdrops = deck.slides
+      .map(createBackdropForSlide);
+
+    deck.on('activate', function() {
+      backdrops.forEach(updateClasses);
+    });
+  };
+};
+
+},{}],2:[function(require,module,exports){
+module.exports = function() {
+  return function(deck) {
+    var addClass = function(el, cls) {
+        el.classList.add('bespoke-' + cls);
+      },
+
+      removeClass = function(el, cls) {
+        el.className = el.className
+          .replace(new RegExp('bespoke-' + cls +'(\\s|$)', 'g'), ' ')
+          .trim();
+      },
+
+      deactivate = function(el, index) {
+        var activeSlide = deck.slides[deck.slide()],
+          offset = index - deck.slide(),
+          offsetClass = offset > 0 ? 'after' : 'before';
+
+        ['before(-\\d+)?', 'after(-\\d+)?', 'active', 'inactive'].map(removeClass.bind(null, el));
+
+        if (el !== activeSlide) {
+          ['inactive', offsetClass, offsetClass + '-' + Math.abs(offset)].map(addClass.bind(null, el));
+        }
+      };
+
+    addClass(deck.parent, 'parent');
+    deck.slides.map(function(el) { addClass(el, 'slide'); });
+
+    deck.on('activate', function(e) {
+      deck.slides.map(deactivate);
+      addClass(e.slide, 'active');
+      removeClass(e.slide, 'inactive');
+    });
+  };
+};
+
+},{}],3:[function(require,module,exports){
+module.exports = function() {
+  return function(deck) {
+    var activeIndex,
+
+      parseHash = function() {
+        var hash = window.location.hash.slice(1),
+          slideNumberOrName = parseInt(hash, 10);
+
+        if (hash) {
+          if (slideNumberOrName) {
+            activateSlide(slideNumberOrName - 1);
+          } else {
+            deck.slides.forEach(function(slide, i) {
+              if (slide.getAttribute('data-bespoke-hash')) {
+                activateSlide(i);
+              }
+            });
+          }
+        }
+      },
+
+      activateSlide = function(index) {
+        if (index !== activeIndex) {
+          deck.slide(index);
+        }
+      };
+
+    setTimeout(function() {
+      parseHash();
+
+      deck.on('activate', function(e) {
+        var slideName = e.slide.getAttribute('data-bespoke-hash');
+        window.location.hash = slideName || e.index + 1;
+        activeIndex = e.index;
+      });
+
+      window.addEventListener('hashchange', parseHash);
+    }, 0);
+  };
+};
+
+},{}],4:[function(require,module,exports){
+module.exports = function(options) {
+  return function(deck) {
+    var isHorizontal = options !== 'vertical';
+
+    document.addEventListener('keydown', function(e) {
+      if (e.which == 34 || // PAGE DOWN
+        e.which == 32 || // SPACE
+        (isHorizontal && e.which == 39) || // RIGHT
+        (!isHorizontal && e.which == 40) // DOWN
+      ) { deck.next(); }
+
+      if (e.which == 33 || // PAGE UP
+        (isHorizontal && e.which == 37) || // LEFT
+        (!isHorizontal && e.which == 38) // UP
+      ) { deck.prev(); }
+    });
+  };
+};
+
+},{}],5:[function(require,module,exports){
+module.exports = function(options) {
+  return function (deck) {
+    var progressParent = document.createElement('div'),
+      progressBar = document.createElement('div'),
+      prop = options === 'vertical' ? 'height' : 'width';
+
+    progressParent.className = 'bespoke-progress-parent';
+    progressBar.className = 'bespoke-progress-bar';
+    progressParent.appendChild(progressBar);
+    deck.parent.appendChild(progressParent);
+
+    deck.on('activate', function(e) {
+      progressBar.style[prop] = (e.index * 100 / (deck.slides.length - 1)) + '%';
+    });
+  };
+};
+
+},{}],6:[function(require,module,exports){
+module.exports = function(options) {
+  return function(deck) {
+    var parent = deck.parent,
+      firstSlide = deck.slides[0],
+      slideHeight = firstSlide.offsetHeight,
+      slideWidth = firstSlide.offsetWidth,
+      useZoom = options === 'zoom' || ('zoom' in parent.style && options !== 'transform'),
+
+      wrap = function(element) {
+        var wrapper = document.createElement('div');
+        wrapper.className = 'bespoke-scale-parent';
+        element.parentNode.insertBefore(wrapper, element);
+        wrapper.appendChild(element);
+        return wrapper;
+      },
+
+      elements = useZoom ? deck.slides : deck.slides.map(wrap),
+
+      transformProperty = (function(property) {
+        var prefixes = 'Moz Webkit O ms'.split(' ');
+        return prefixes.reduce(function(currentProperty, prefix) {
+            return prefix + property in parent.style ? prefix + property : currentProperty;
+          }, property.toLowerCase());
+      }('Transform')),
+
+      scale = useZoom ?
+        function(ratio, element) {
+          element.style.zoom = ratio;
+        } :
+        function(ratio, element) {
+          element.style[transformProperty] = 'scale(' + ratio + ')';
+        },
+
+      scaleAll = function() {
+        var xScale = parent.offsetWidth / slideWidth,
+          yScale = parent.offsetHeight / slideHeight;
+
+        elements.forEach(scale.bind(null, Math.min(xScale, yScale)));
+      };
+
+    window.addEventListener('resize', scaleAll);
+    scaleAll();
+  };
+
+};
+
+},{}],7:[function(require,module,exports){
+module.exports = function(options) {
+  return function(deck) {
+    var axis = options == 'vertical' ? 'Y' : 'X',
+      startPosition,
+      delta;
+
+    deck.parent.addEventListener('touchstart', function(e) {
+      if (e.touches.length == 1) {
+        startPosition = e.touches[0]['page' + axis];
+        delta = 0;
+      }
+    });
+
+    deck.parent.addEventListener('touchmove', function(e) {
+      if (e.touches.length == 1) {
+        e.preventDefault();
+        delta = e.touches[0]['page' + axis] - startPosition;
+      }
+    });
+
+    deck.parent.addEventListener('touchend', function() {
+      if (Math.abs(delta) > 50) {
+        deck[delta > 0 ? 'prev' : 'next']();
+      }
+    });
+  };
+};
+
+},{}],8:[function(require,module,exports){
+var from = function(selectorOrElement, plugins) {
+  var parent = selectorOrElement.nodeType === 1 ? selectorOrElement : document.querySelector(selectorOrElement),
+    slides = [].filter.call(parent.children, function(el) { return el.nodeName !== 'SCRIPT'; }),
+    activeSlide = slides[0],
+    listeners = {},
+
+    activate = function(index, customData) {
+      if (!slides[index]) {
+        return;
+      }
+
+      fire('deactivate', createEventData(activeSlide, customData));
+      activeSlide = slides[index];
+      fire('activate', createEventData(activeSlide, customData));
+    },
+
+    slide = function(index, customData) {
+      if (arguments.length) {
+        fire('slide', createEventData(slides[index], customData)) && activate(index, customData);
+      } else {
+        return slides.indexOf(activeSlide);
+      }
+    },
+
+    step = function(offset, customData) {
+      var slideIndex = slides.indexOf(activeSlide) + offset;
+
+      fire(offset > 0 ? 'next' : 'prev', createEventData(activeSlide, customData)) && activate(slideIndex, customData);
+    },
+
+    on = function(eventName, callback) {
+      (listeners[eventName] || (listeners[eventName] = [])).push(callback);
+
+      return function() {
+        listeners[eventName] = listeners[eventName].filter(function(listener) {
+          return listener !== callback;
+        });
+      };
+    },
+
+    fire = function(eventName, eventData) {
+      return (listeners[eventName] || [])
+        .reduce(function(notCancelled, callback) {
+          return notCancelled && callback(eventData) !== false;
+        }, true);
+    },
+
+    createEventData = function(el, eventData) {
+      eventData = eventData || {};
+      eventData.index = slides.indexOf(el);
+      eventData.slide = el;
+      return eventData;
+    },
+
+    deck = {
+      on: on,
+      fire: fire,
+      slide: slide,
+      next: step.bind(null, 1),
+      prev: step.bind(null, -1),
+      parent: parent,
+      slides: slides
+    };
+
+  (plugins || []).forEach(function(plugin) {
+    plugin(deck);
+  });
+
+  activate(0);
+
+  return deck;
+};
+
+module.exports = {
+  from: from
+};
+
+},{}],9:[function(require,module,exports){
+var bespoke = require('bespoke');
+
+var classes = require('bespoke-classes');
+var keys = require('bespoke-keys');
+var touch = require('bespoke-touch');
+var backdrop = require('bespoke-backdrop');
+var scale = require('bespoke-scale');
+var hash = require('bespoke-hash');
+var progress = require('bespoke-progress');
+
+//var highres = require('./highres');
+
+module.exports = bespoke.from('#slides', [
+  classes(),
+  keys(),
+  touch(),
+  backdrop(),
+  scale(),
+  hash(),
+  progress(),
+])
+;
+
+// hack to get scale transform to work
+(function fireResize () {
+  var evt = document.createEvent('UIEvents');
+  evt.initUIEvent('resize', true, false, window, 0);
+  window.dispatchEvent(evt);
+})();
+
+},{"bespoke":8,"bespoke-backdrop":1,"bespoke-classes":2,"bespoke-hash":3,"bespoke-keys":4,"bespoke-progress":5,"bespoke-scale":6,"bespoke-touch":7}]},{},[9]);

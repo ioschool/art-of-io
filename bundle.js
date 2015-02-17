8bd5d1da9c80a5fce1a979c3a7c7656ff750aad0
Michael Williams <dinosaur@riseup.net>
2015-02-17 18:02:10 +1300

    Updates

--- bundle.js
+++ bundle.js
@@ -15086,19 +15086,16 @@ query.notes = safeEval(query.notes);
 function queryize (params) {
   var anchors = document.querySelectorAll('a[data-query]');
   [].forEach.call(anchors, function (el) {
-    console.log(el.href, el.dataset.query);
     var dataQuery = el.dataset.query;
     if (dataQuery === '.') {
       el.setAttribute('href', el.getAttribute('href') + search);
     } else {
-      console.log("safeEval", dataQuery, params);
       var context = extend(params);
       safeEval("__out = " + dataQuery, context);
       var query = context.__out;
       console.log("query", query);
       el.setAttribute('href', el.getAttribute('href') + "?" + QS.stringify(query));
     }
-    console.log(el.href, el.dataset.query);
   });
 
 }
@@ -15108,7 +15105,9 @@ queryize(query);
 if (query.notes) {
   document.querySelector('main').classList.add('container');
 } else {
-  document.querySelector('.notes-only').classList.add('hidden');
+  var notesOnly = document.querySelector('.notes-only');
+  if (notesOnly) notesOnly.classList.add('hidden');
+
   bespoke.from('.presentation', [
     require('bespoke-classes')(),
     require('bespoke-keys')(),
